9941a85fba9e361b2edd129575197ec50142e431
MadJiGit <raykovmladen@gmail.com>
2021-02-11 22:41:29 +0200

    remove saveUserData logic fromGameData and added same logic to UserData class

---
+++ Classes/testO.cpp
@@ -0,0 +1,8 @@
+//
+//  testO.cpp
+//  Shark-mobile
+//
+//  Created by Madji on 11.02.21.
+//
+
+#include "testO.hpp"
---
+++ Classes/testO.hpp
@@ -0,0 +1,13 @@
+//
+//  testO.hpp
+//  Shark-mobile
+//
+//  Created by Madji on 11.02.21.
+//
+
+#ifndef testO_hpp
+#define testO_hpp
+
+#include <stdio.h>
+
+#endif /* testO_hpp */
